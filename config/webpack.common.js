06450a005afa6498768e95a2e96460184c99a1fb
Xavier-LEPLATRE <xavier.graphdesign@gmail.com>
2022-02-16 14:11:21 +0100

    Modif de scss

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -42,7 +42,7 @@ module.exports = {
 
     new HtmlWebpackPlugin({
       favicon: paths.assets + '/favicon.ico',
-      template: paths.root + '/index.html',
+      template: paths.assets + '/index.html',
     }),
   ],
 
